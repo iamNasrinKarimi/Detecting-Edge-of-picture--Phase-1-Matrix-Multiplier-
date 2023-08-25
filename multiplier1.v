b546c2196e3133112fa2e9d696df697766e122cf
Nasrin Karimi <116264281+iamNasrinKarimi@users.noreply.github.com>
2023-08-25 19:04:12 +0330

    Add files via upload

---
+++ multiplier1.v
@@ -0,0 +1,52 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    23:21:42 12/04/2020 
+// Design Name: 
+// Module Name:    multiplier1 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module multiplier1 #(parameter W_IN=8 , W_OUT=16 , ROWS_A=2 , COLS_A=1 ,
+ROWS_B=1 , COLS_B=3 )
+(input data_in, input mem_sel, input reset, input clk,
+input signed[W_IN-1 : 0]data ,input [1 : 0]row_in, input [2 : 0]col_in, input [1 :
+0]row_out, input [2 : 0]col_out,
+output reg signed [W_OUT-1 : 0]out
+ );
+reg signed[7:0]A[1:0];
+reg signed[7:0]B[2:0];
+reg signed[15:0]result[1:0][2:0];
+integer i,j;
+always @ (posedge clk)
+if(reset==0 && data_in==1)begin
+if(mem_sel==0)begin
+A[row_in] <=data;
+end
+if(mem_sel==1)begin
+B[col_in]<=data;
+end
+for(i=0;i<2;i=i+1)begin
+for(j=0;j<3;j=j+1)begin
+result[i][j] =A[i]*B[j];
+end
+end
+end
+always @ (posedge clk)
+if(reset==0 && data_in==1)begin
+out=result[row_out][col_out];
+end else begin
+out=0;
+end
+endmodule
---
+++ test_multiplier1.v
@@ -0,0 +1,116 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   23:23:38 12/04/2020
+// Design Name:   multiplier1
+// Module Name:   C:/.Xilinx/matrix_multiplier1/test_multiplier1.v
+// Project Name:  matrix_multiplier1
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: multiplier1
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module test_multiplier1;
+
+// Inputs
+reg data_in;
+reg mem_sel;
+reg reset;
+reg clk;
+reg [7:0] data;
+reg [1:0] row_in;
+reg [2:0] col_in;
+reg [1:0] row_out;
+reg [2:0] col_out;
+// Outputs
+wire [15:0] out;
+// Instantiate the Unit Under Test (UUT)
+matrix_multiplier1 uut (
+.data_in(data_in),
+.mem_sel(mem_sel),
+.reset(reset),
+.clk(clk),
+.data(data), 
+.row_in(row_in),
+.col_in(col_in),
+.row_out(row_out),
+.col_out(col_out),
+.out(out)
+);
+initial begin
+#20;
+clk=1;
+forever begin
+#10 clk =!clk;
+end
+end
+initial begin
+#20;
+data_in = 1;
+mem_sel = 0;
+reset = 0;
+data = $random;
+row_in = 0;
+col_in = 0;
+#20;
+data_in = 1;
+mem_sel = 0;
+reset = 0;
+data = $random;
+row_in = 1;
+col_in = 0;
+#20;
+data_in = 1;
+mem_sel = 1;
+reset = 0;
+data = $random;
+row_in = 0;
+col_in = 0;
+#20;
+data_in = 1;
+mem_sel = 1;
+reset = 0;
+data = $random;
+row_in = 0;
+col_in =1;
+#20;
+data_in = 1;
+mem_sel = 1;
+reset = 0;
+data = $random;
+row_in = 0;
+col_in = 2;
+#20;
+row_out=0;
+col_out=0;
+#20;
+row_out=0;
+col_out=1;
+#20;
+row_out=0;
+col_out=2;
+#20;
+row_out=1;
+col_out=0;
+#20;
+row_out=1;
+col_out=1;
+#20;
+row_out=1;
+col_out=2;
+end
+
+endmodule
+
